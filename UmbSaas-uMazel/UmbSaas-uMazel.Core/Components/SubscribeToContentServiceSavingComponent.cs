0e28676256ae9632eef1486e6337103bcdb00153
Aaron Sadler <aaron@aaronsadler.co.uk>
2021-05-13 23:36:06 +0100

    Added Imageprocessor modules

---
+++ UmbSaas-uMazel/UmbSaas-uMazel.Core/Components/SubscribeToContentServiceSavingComponent.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Web;
+using ImageProcessor.Web.Helpers;
+using ImageProcessor.Web.HttpModules;
+
+namespace UmbSaas_uMazel.Core.Components
+{
+    public class SubscribeToContentServiceSavingComponent : Umbraco.Core.Composing.IComponent
+    {
+        public void Initialize()
+        {
+            ImageProcessingModule.ValidatingRequest += ImageProcessingModule_ValidatingRequest;
+        }
+
+        private void ImageProcessingModule_ValidatingRequest(object sender, ValidatingRequestEventArgs e)
+        {
+            if (!e.Context.Request.Url.AbsolutePath.EndsWith(".gif"))
+            {
+                var queryString = HttpUtility.ParseQueryString(e.QueryString);
+                if (e.Context.Request.AcceptTypes != null && e.Context.Request.AcceptTypes.Contains("image/webp"))
+                {
+                    queryString.Remove("format");
+                    queryString["format"] = "webp";
+                    if (queryString.Get("quality") != null)
+                    {
+                        string quality = queryString["quality"];
+                        queryString.Remove("quality");
+                        queryString["quality"] = quality;
+                    }
+                }
+                e.QueryString = queryString.ToString();
+            }
+        }
+
+        public void Terminate()
+        {
+        }
+    }
+}
---
+++ UmbSaas-uMazel/UmbSaas-uMazel.Core/Composers/SubscribeToContentServiceSavingComposer.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Composing;
+using UmbSaas_uMazel.Core.Components;
+
+namespace UmbSaas_uMazel.Core.Composers
+{
+    public class SubscribeToContentServiceSavingComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Components().Append<SubscribeToContentServiceSavingComponent>();
+        }
+    }
+}
